4d7b4df14c61fd621e5c2819fec2cfc1132981a6
neko <gregoire.doebele@ens-paris-saclay.fr>
2022-11-22 12:46:15 +0100

    debut projection

---
+++ projection.c
@@ -0,0 +1,20 @@
+#include <cblas.h>
+
+struct B_proj {
+	double *Bm;
+	double *Bm1;
+};
+
+// A : taille n*n
+// B : taille m*m
+struct B_proj projection(double *A, int n, int m)
+{
+	// alloc B ...
+	struct B_proj B;
+}
+
+
+// int main()
+// {
+
+// }
