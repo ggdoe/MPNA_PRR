4371e5ae77b920d865bc3da6620810b39e34a870
VIDAL-Antoine <avidal78390@gmail.com>
2023-01-27 17:14:17 +0100

    Ajout matrices

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ module load intel/20.0.4/gcc-4.8.5 intel-mkl/2020.4.304/intel-20.0.4.304 intel-m
 module load gcc/11.2.0/gcc-4.8.5 openmpi/4.1.1/gcc-11.2.0 openblas/0.3.8/gcc-9.2.0
 
 FILES="src/*.c"
-ICC_FLAGS=-Wall -std=c11 -flto -O3 -mkl -DMKL
-GCC_FLAGS=-flto -O3 -lm -lopenblas 
+ICC_FLAGS="-Wall -std=c11 -flto -O3 -mkl -DMKL"
+GCC_FLAGS="-flto -O3 -lm -lopenblas"
 #-no-multibyte-chars
 
 echo prr_icc
